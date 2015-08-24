b7ad6a40f7fae8d9aac687d1e95a340ef2957876
QuestorX <questorx@friends-at-net.de>
2015-08-24 13:57:46 +0200

    new constants added

--- RelationshipColumnView/core/constant_api.php
+++ RelationshipColumnView/core/constant_api.php
@@ -1,13 +1,18 @@
 <?php
 
+define ('RELATIONSHIPCOLUMNVIEW_CORE_FOLDER', 'core');
+
 // URL to RelationshipColumnView plugin
-define ('RELATIONSHIPCOLUMNVIEW_PLUGIN_URL', config_get_global ('path') . 'plugins/' . plugin_get_current () . '/');
+define ('RELATIONSHIPCOLUMNVIEW_PLUGIN_URL', config_get_global ('path') . 'plugins' . DIRECTORY_SEPARATOR . plugin_get_current () . DIRECTORY_SEPARATOR);
+
+// URL to RelationshipColumnView plugin/core
+define ('RELATIONSHIPCOLUMNVIEW_CORE_RELATIVE_URL', 'plugins' . DIRECTORY_SEPARATOR . plugin_get_current () . DIRECTORY_SEPARATOR . RELATIONSHIPCOLUMNVIEW_CORE_FOLDER . DIRECTORY_SEPARATOR);
 
 // Path to RelationshipColumnView plugin folder
 define ('RELATIONSHIPCOLUMNVIEW_PLUGIN_URI', config_get_global ('plugin_path') . plugin_get_current () . DIRECTORY_SEPARATOR);
 
 // Path to RelationshipColumnView core folder
-define ('RELATIONSHIPCOLUMNVIEW_CORE_URI', RELATIONSHIPCOLUMNVIEW_PLUGIN_URI . 'core' . DIRECTORY_SEPARATOR);
+define ('RELATIONSHIPCOLUMNVIEW_CORE_URI', RELATIONSHIPCOLUMNVIEW_PLUGIN_URI . RELATIONSHIPCOLUMNVIEW_CORE_FOLDER . DIRECTORY_SEPARATOR);
 
 // Default treshold level
 define ('PLUGINS_RELATIONSHIPCOLUMNVIEW_THRESHOLD_LEVEL_DEFAULT', ADMINISTRATOR);
